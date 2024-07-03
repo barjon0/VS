b8522e7872b6068c29701fe161f9f3b96765f001
Philipp Treutlein <ph.treutlein@web.de>
2024-07-03 13:29:14 +0200

    changed classifier to desiciontree, added batch classification

--- Mir.py
+++ Mir.py
@@ -4,17 +4,19 @@ import os
 import re
 # from sklearn.ensemble import RandomForestClassifier
 from torchvggish import vggish, vggish_input
-from sklearn import svm
+from sklearn.tree import DecisionTreeClassifier
 import numpy as np
+from pprint import pprint
 
 # duration = ??
 # sampling rate?
 N, H = 4096, 1024
 TRAIN_MUSIC_DIR = "music/train"
+TEST_MUSIC_DIR="music/test"
 
 EMBEDDING_MODEL = vggish()
 EMBEDDING_MODEL.eval()
-FEATURE_LENGTH = 218
+FEATURE_LENGTH = 170 # TODO: search way to do this automatically, instead of by trial-and-error
 
 
 def readFolder2Embedding(path):
@@ -27,6 +29,7 @@ def readFolder2Embedding(path):
     labels = []
 
     for i, song in enumerate(songs):
+        print(f"Learning Embedding for {song}")
         embedding = EMBEDDING_MODEL.forward(
             vggish_input.wavfile_to_examples(path+"/"+song.decode("utf-8")))
         # normalize length, convert to numpy array and flatten the feature array
@@ -48,21 +51,35 @@ def readIn():
             TRAIN_MUSIC_DIR+"/"+directory)
         data = np.concatenate((data, dir_data), axis=0)
         label += dir_label
+    print("All data read in successfully!")
     return data, label
 
 
-def classify(classifier, path):
-    embedding = EMBEDDING_MODEL.forward(vggish_input.wavfile_to_examples(path))
+def classify(classifier, filepath):
+    print("Predicting label for" + filepath + "...")
+    embedding = EMBEDDING_MODEL.forward(vggish_input.wavfile_to_examples(filepath))
     converted_embedding = embedding.detach().numpy()[
         :FEATURE_LENGTH, :].flatten()
     return classifier.predict(converted_embedding.reshape(1, -1))
+
+def classifyBatch(classifier, directory_path):
+    directory = os.fsencode(directory_path)
+    songs = os.listdir(directory)
+    res=[]
+    for song in songs:
+        embedding = EMBEDDING_MODEL.forward(vggish_input.wavfile_to_examples(directory_path+"/"+song.decode("utf-8")))
+        converted_embedding = embedding.detach().numpy()[:FEATURE_LENGTH, :].flatten()
+        predicted_class=classifier.predict(converted_embedding.reshape(1, -1))
+        res.append({"song":song, "predicted_class": predicted_class[0]})
+    return res
 
 
 if __name__ == "__main__":
 
     data, label = readIn()
 
-    classifier = svm.SVC()
+    classifier=DecisionTreeClassifier(max_depth=5)
     classifier.fit(data, label)
+    print("Training successful")
 
-    # print(classify(classifier, ""))
+    pprint(classifyBatch(classifier, TEST_MUSIC_DIR))
